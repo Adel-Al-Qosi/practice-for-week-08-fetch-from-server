ce52bf2dedc55a3cdd3a7ea569dd68d286579205
Adel-Al-Qosi <Adel.Al.Qosi@gmail.com>
2023-04-05 11:00:04 +0300

    finish practice

--- assets/js/your-code.js
+++ assets/js/your-code.js
@@ -1,19 +1,75 @@
 export function getAllDogs() {
-    // Your code here
+  // Your code here
+  const url = "/dogs";
+  const headers = { "Content-Type": "text/html" };
+  const body = new URLSearchParams({
+    key: "value",
+  });
+  const options = {
+    method: "GET",
+    headers: headers,
+    body: body,
+  };
+
+  return fetch(
+    url,
+    new URLSearchParams({
+      method: "GET",
+      headers: headers,
+    })
+  );
 }
 
 export function getDogNumberTwo() {
-    // Your code here
+  // Your code here
+
+  return fetch(
+    "/dogs/2",
+    new URLSearchParams({
+      method: "GET",
+      headers: { "Content-Type": "text/html" },
+    })
+  );
 }
 
 export function postNewDog() {
-    // Your code here
+  // Your code here
+  return fetch(
+    "/dogs/new",
+    new URLSearchParams({
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    })
+  );
 }
 
 export function postNewDogV2(name, age) {
-     // Your code here
+  const url = "/dogs";
+  const headers = { "Content-Type": "application/x-www-form-urlencoded" };
+  const body = new URLSearchParams({
+    name: name,
+    age: age,
+  });
+  const options = {
+    method: "POST",
+    headers: headers,
+    body: body,
+  };
+  return fetch(url, options);
 }
 
 export function deleteDog(id) {
-      // Your code here
+  const url = `/dogs/${id}/delete`;
+  const headers = { "AUTH": "ckyut5wau0000jyv5bsrud90y", };
+  // const body = new URLSearchParams({
+  //   id: id
+  // });
+  const options = {
+    method: "POST",
+    headers: headers,
+    //body: body,
+  };
+  return fetch(url, options);
 }
